3a0d61f805b0589d3f1db7ffa07f8ef3d404bc03
Damian <josepwnz@gmail.com>
2025-06-16 23:28:54 +0200

    redirect to chats when press join on some group

--- src/views/groups/GroupListView.vue
+++ src/views/groups/GroupListView.vue
@@ -4,6 +4,10 @@ import type { Group } from "@/types/Group";
 import type { Chat, ChatType } from "@/types/Chat";
 import { useGroupStore } from "@/stores/group";
 import { useChatStore } from "@/stores/chat";
+import { useCustomerStore } from "@/stores/customer";
+import { useRoute, useRouter } from "vue-router";
+const customerStore = useCustomerStore();
+const router = useRouter();
 const groupStore = useGroupStore();
 const chatStore = useChatStore();
 const groups = computed(() => groupStore.getGroups as Group[]);
@@ -13,19 +17,12 @@ function joinGroup(id: number) {
   const chat = {
     name: group?.name,
     type: "GROUP",
-    history: [
-      {
-        sender: "DAMIAN",
-        message: "Hey!",
-      },
-      {
-        sender: "RONALD",
-        message: "Hello!",
-      },
-    ],
-    participants: [{ name: "RONALD" }],
+    history: [],
+    participants: [],
   } as Chat;
   chatStore.addChat(chat);
+  // TODO setselectedchat
+  router.push("/chats");
 }
 </script>
 <template>
@@ -43,7 +40,7 @@ function joinGroup(id: number) {
         class="rounded bg-gray-300 p-4 flex justify-between items-start"
       >
         <div class="flex flex-col flex-1">
-          <span class="font-semibold">{{ group.id }} - {{ group.name }}</span>
+          <span class="font-semibold">{{ group.name }}</span>
           <span class="text-xs text-gray-700">{{ group.description }}</span>
         </div>
 
