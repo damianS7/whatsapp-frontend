3ed17f0a5b2b67496a90a4b4d872b7fcc5cd0c65
Damian <josepwnz@gmail.com>
2025-06-22 21:15:51 +0200

    code cleanup

--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -4,6 +4,8 @@ import { Client, CompatClient, Stomp } from "@stomp/stompjs";
 import { Chat } from "@/types/Chat";
 import { ChatMessage } from "@/types/ChatMessage";
 import { useCustomerStore } from "@/stores/customer";
+import { useChat } from "@/composables/useChat";
+const { generateChatId } = useChat();
 
 export const useChatStore = defineStore("chat", {
   state: () => ({
@@ -30,12 +32,9 @@ export const useChatStore = defineStore("chat", {
       };
     },
     getChatById: (state) => {
-      return (id: number) => {
+      return (id: string) => {
         return state.chats.find((chat) => {
-          if (chat.type === "PRIVATE") {
-            return chat.toCustomerId === id;
-          }
-          return chat.groupId === id;
+          return chat.id === id;
         });
       };
     },
@@ -52,19 +51,14 @@ export const useChatStore = defineStore("chat", {
     },
     async addChat(newChat: Chat) {
       const chatExists = this.chats.find((chat) => chat.name === newChat.name);
-      if (!chatExists) {
-        this.chats.push(newChat);
-      }
-      this.saveChatState();
-
-      const chatId =
-        newChat.type === "PRIVATE" ? newChat.toCustomerId : newChat.groupId;
-
-      if (!chatId) {
+      if (chatExists) {
         return;
       }
 
-      await this.subscribeToChat(newChat.type, chatId);
+      this.chats.push(newChat);
+      this.saveChatState();
+
+      await this.subscribeToChat(newChat.id);
     },
     async deleteChat(chatName: string) {
       const index = this.chats.findIndex((chat) => chat.name === chatName);
@@ -72,61 +66,79 @@ export const useChatStore = defineStore("chat", {
       this.saveChatState();
       // this.unSuscribeFromChat(chatName);
     },
-    async sendMessage(
-      chatType: string,
-      chatName: string,
-      message: ChatMessage
-    ) {
+    async sendMessage(message: ChatMessage) {
       if (!this.stompClient) {
         return;
       }
 
       this.stompClient.send(
-        `/app/chat.send.${chatType}.${chatName}`,
+        `/app/chat.send.${message.chatId}`,
         {},
         JSON.stringify(message)
       );
       this.saveChatState();
     },
-    async subscribeToChat(chatType: string, chatId: number) {
+    handleMessage(chatMessage: ChatMessage) {
+      console.log(
+        `Received message for chat: ${chatMessage.chatId}`,
+        chatMessage
+      );
+      const customerStore = useCustomerStore();
+
+      // if chatMessage its a private message ...
+      if (chatMessage.chatType === "PRIVATE" && chatMessage.toCustomerId) {
+        // if the message its sent to the logged customer
+        if (
+          chatMessage.toCustomerId === customerStore.getLoggedCustomer.id &&
+          chatMessage.fromCustomerId
+        ) {
+          chatMessage.chatId = generateChatId(
+            "PRIVATE",
+            chatMessage.fromCustomerId
+          );
+        }
+
+        // Create a new chat if it doesn't exist
+        if (!this.getChatById(chatMessage.chatId)) {
+          const newChat: Chat = {
+            id: chatMessage.chatId,
+            name: chatMessage.fromCustomerName,
+            type: "PRIVATE",
+            history: [],
+            participants: [
+              {
+                customerId: chatMessage.fromCustomerId,
+                customerName: chatMessage.fromCustomerName,
+                customerAvatar: "",
+              },
+              {
+                customerId: customerStore.getLoggedCustomer.id,
+                customerName: customerStore.getLoggedCustomer.profile.firstName,
+                customerAvatar:
+                  customerStore.getLoggedCustomer.profile.avatarFilename || "",
+              },
+            ],
+          };
+          this.chats.push(newChat);
+          this.subscribeToChat(newChat.id);
+        }
+      }
+
+      this.getChatById(chatMessage.chatId)?.history.push(chatMessage);
+      this.saveChatState();
+    },
+    async subscribeToChat(chatId: string) {
       if (!this.stompClient) {
         return;
       }
 
-      this.stompClient.subscribe(
-        `/topic/chat.${chatType}.${chatId}`,
-        (message) => {
-          const chatMessage = JSON.parse(message.body) as ChatMessage;
-          console.log(
-            `Received message for chat: ${chatType}.${chatId}`,
-            chatMessage
-          );
-
-          if (chatType === "PRIVATE" && chatMessage.toCustomerId) {
-            chatId = chatMessage.toCustomerId;
-
-            if (
-              chatMessage.toCustomerId ===
-                useCustomerStore().getLoggedCustomer.id &&
-              chatMessage.fromCustomerId
-            ) {
-              chatId = chatMessage.fromCustomerId;
-            }
-
-            this.getChatById(chatId)?.history.push(
-              JSON.parse(message.body) as ChatMessage
-            );
-          }
-
-          if (chatType === "GROUP" && chatMessage.groupId) {
-            this.getChatById(chatMessage.groupId)?.history.push(
-              JSON.parse(message.body) as ChatMessage
-            );
-          }
-          this.saveChatState();
+      this.stompClient.subscribe(`/topic/chat.${chatId}`, (message) => {
+        const chatMessage = JSON.parse(message.body) as ChatMessage;
+        if (!chatMessage.chatId) {
+          return;
         }
-      );
-      // console.log(`Subscribed to chat: /topic/chat.${chatType}.${chatId}`);
+        this.handleMessage(chatMessage);
+      });
     },
     async unSuscribeFromChat(chatName: string) {
       if (!this.stompClient) {
@@ -160,18 +172,12 @@ export const useChatStore = defineStore("chat", {
         { Authorization: `Bearer ${token}` },
         async () => {
           for (const chat of this.chats) {
-            const chatId =
-              chat.type === "PRIVATE" ? chat.toCustomerId : chat.groupId;
-
-            if (chatId) {
-              await this.subscribeToChat(chat.type, chatId);
-            }
+            await this.subscribeToChat(chat.id);
           }
 
           // Subscribe to the logged-in customer's private chat
           await this.subscribeToChat(
-            "PRIVATE",
-            customerStore.getLoggedCustomer.id
+            generateChatId("PRIVATE", customerStore.getLoggedCustomer.id)
           );
         }
       );
