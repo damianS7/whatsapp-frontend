0533cb19ee5000b5606077e1d746f1706b361d10
Damian <josepwnz@gmail.com>
2025-09-30 22:44:58 +0200

    Final version of CustomAlert. Added fading and default options

--- src/components/CustomAlert.vue
+++ src/components/CustomAlert.vue
@@ -4,92 +4,92 @@ import AlertTitle from "@/components/ui/alert/AlertTitle.vue";
 import AlertDescription from "@/components/ui/alert/AlertDescription.vue";
 import { AlertType } from "@/types/AlertType";
 import { X } from "lucide-vue-next";
-import { ref, type Ref } from "vue";
-// TODO fadeout
-// TODO simplify and rename interfaces
-// TODO exceptiosn
-export interface CustomAlertType {
-  message: string;
-  type: AlertType;
-  timeout: number;
-  visible: boolean;
-  closable: boolean;
-  errors: Record<string, string[]>;
-  title: string;
-  // options: CustomAlertOptions
-}
+import { ref } from "vue";
+import { ApiError } from "@/types/ApiError";
 
-export interface CustomAlertOptions {
-  errors?: Record<string, string[]>;
-  title?: string;
+export interface AlertOptions {
   type?: AlertType;
+  title?: string;
   timeout?: number;
   closable?: boolean;
 }
 
 const alert = ref({
-  message: "",
-  errors: {},
-  title: "",
   type: AlertType.INFO,
+  title: "",
+  message: "",
   timeout: 0,
-  visible: false,
   closable: true,
-}) as Ref<CustomAlertType>;
+  errors: {},
+  visible: false,
+  fading: false,
+});
 
 // props
-function success(message: string, options?: CustomAlertOptions) {
-  if (!options) {
-    options = defaultOptions(AlertType.SUCCESS);
-  }
+function success(message: string, options?: AlertOptions) {
+  options = validateOptions(AlertType.SUCCESS, options);
 
   show(message, options);
 }
 
-function info(message: string, options?: CustomAlertOptions) {
-  if (!options) {
-    options = defaultOptions(AlertType.INFO);
-  }
+function info(message: string, options?: AlertOptions) {
+  options = validateOptions(AlertType.INFO, options);
 
   show(message, options);
 }
 
-function error(message: string, options?: CustomAlertOptions) {
-  if (!options) {
-    options = defaultOptions(AlertType.ERROR);
-  }
+function error(message: string, options?: AlertOptions) {
+  options = validateOptions(AlertType.ERROR, options);
 
   show(message, options);
 }
 
-function exception(message: string, options?: CustomAlertOptions) {
-  if (!options) {
-    options = defaultOptions(AlertType.ERROR);
+function exception(exception: any, options?: AlertOptions) {
+  options = validateOptions(AlertType.ERROR, options);
+
+  if (exception instanceof ApiError) {
+    show(exception.message, options);
+    alert.value.errors = exception.errors ?? {};
+    return;
   }
-  show(message, options);
+
+  if (exception && exception.message) {
+    show(exception.message || "", options);
+    return;
+  }
+
+  show("Unkown error exception", options);
 }
 
 // internal
-function defaultOptions(type: AlertType): CustomAlertOptions {
-  let options = {
-    type,
-  } as CustomAlertOptions;
+function validateOptions(
+  type: AlertType,
+  options?: AlertOptions
+): AlertOptions {
+  if (!options) {
+    options = {} as AlertOptions;
+  }
 
-  options.title ||= {
-    [AlertType.INFO]: "Info",
-    [AlertType.ERROR]: "Error",
-    [AlertType.SUCCESS]: "Success",
-  }[type];
+  options.closable = options.closable ?? true;
+  options.type = options.type ?? type;
+  options.timeout = options.timeout ?? 0;
+  options.title =
+    options.title ??
+    {
+      [AlertType.INFO]: "Info",
+      [AlertType.ERROR]: "Error",
+      [AlertType.SUCCESS]: "Success",
+    }[type];
 
   return options;
 }
 
-function show(message: string, options: CustomAlertOptions) {
+function show(message: string, options: AlertOptions) {
+  alert.value.message = message;
+  alert.value.title = options.title ?? alert.value.title;
   alert.value.type = options.type ?? alert.value.type;
   alert.value.timeout = options.timeout ?? alert.value.timeout;
   alert.value.closable = options.closable ?? alert.value.closable;
-  alert.value.errors = options.errors ?? alert.value.errors;
-  alert.value.message = message;
   alert.value.visible = true;
 
   if (alert.value.timeout && alert.value.timeout > 1) {
@@ -100,19 +100,19 @@ function show(message: string, options: CustomAlertOptions) {
 }
 
 function hide() {
-  alert.value.visible = false;
-  // alert.value.message = "";
+  alert.value.fading = true;
 
-  // setTimeout(() => {
-  //   alert.value.visible = false;
-  // }, 1 * 1000);
+  setTimeout(() => {
+    alert.value.visible = false;
+    alert.value.fading = false;
+  }, 500);
 }
 
 defineExpose({ error, info, success, exception });
 </script>
 <template>
   <Alert
-    v-if="alert.visible && alert.message"
+    v-if="alert.visible"
     v-bind="$attrs"
     class="transition-opacity duration-500"
     :class="[
@@ -122,13 +122,18 @@ defineExpose({ error, info, success, exception });
         'bg-red-100 border-red-400 text-red-700',
       alert.type === AlertType.SUCCESS &&
         'bg-green-100 border-green-400 text-green-700',
-      alert.visible ? 'opacity-100' : 'opacity-0',
+      alert.fading ? 'opacity-0' : 'opacity-100',
     ]"
   >
     <AlertTitle>
       <div class="flex items-center justify-between w-full">
         <span>{{ alert.title }}</span>
-        <button type="button" class="cursor-pointer" @click="hide()">
+        <button
+          v-if="alert.closable"
+          type="button"
+          class="cursor-pointer"
+          @click="hide()"
+        >
           <X class="h-4 w-4" />
         </button>
       </div>
