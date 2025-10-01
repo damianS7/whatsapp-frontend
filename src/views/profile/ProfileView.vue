cb9db8e40b64bd93f426311da871652f03b4fadc
Damian <josepwnz@gmail.com>
2025-10-02 00:56:24 +0200

    removed modal. made field computed so its synced with store

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
 import { useUserStore } from "@/stores/user";
 import CustomAlert from "@/components/CustomAlert.vue";
-import { onMounted, ref } from "vue";
+import { computed, ref } from "vue";
 import ProfileEditableField from "./components/ProfileEditableField.vue";
 import ProfilePhoto from "./components/ProfilePhotoUploader.vue";
 import type { GenderType } from "@/types/User";
 import { useModalStore } from "@/stores/modal";
-import ConfirmMessageModal from "@/components/modal/ConfirmMessageModal.vue";
 
 // store
 const modalStore = useModalStore();
 const userStore = useUserStore();
-const user = userStore.getLoggedUser;
 const genderTypes: GenderType[] = ["MALE", "FEMALE"];
 const genderOptions = genderTypes.map((value) => ({
   value,
@@ -23,12 +21,12 @@ const genderOptions = genderTypes.map((value) => ({
 const alert = ref();
 
 // updatable fields to be displayed
-const formFields = ref([
+const formFields = computed(() => [
   {
     name: "firstName",
     type: "text",
     placeholder: "First name",
-    value: userStore.user.firstName,
+    value: userStore.getLoggedUser.firstName,
     error: "",
     isEditing: false,
     edited: false,
@@ -38,7 +36,7 @@ const formFields = ref([
     name: "lastName",
     type: "text",
     placeholder: "Last name",
-    value: user.lastName,
+    value: userStore.getLoggedUser.lastName,
     error: "",
     isEditing: false,
     edited: false,
@@ -47,7 +45,7 @@ const formFields = ref([
     name: "email",
     type: "email",
     placeholder: "Email",
-    value: user.email,
+    value: userStore.getLoggedUser.email,
     error: "",
     isEditing: false,
     edited: false,
@@ -56,7 +54,7 @@ const formFields = ref([
     name: "phone",
     type: "text",
     placeholder: "Phone",
-    value: user.phone,
+    value: userStore.getLoggedUser.phone,
     error: "",
     isEditing: false,
     edited: false,
@@ -65,7 +63,7 @@ const formFields = ref([
     name: "gender",
     type: "select",
     placeholder: "Gender",
-    value: user.gender,
+    value: userStore.getLoggedUser.gender,
     options: genderOptions,
     error: "",
     isEditing: false,
@@ -75,7 +73,7 @@ const formFields = ref([
     name: "birthdate",
     type: "date",
     placeholder: "Birthdate",
-    value: user.birthdate,
+    value: userStore.getLoggedUser.birthdate,
     error: "",
     isEditing: false,
     edited: false,
@@ -109,7 +107,6 @@ async function updateField(
   }
 
   // wait for the user to input his password
-  // const currentPassword = await modals.confirmPassword.value.open();
   const currentPassword = (await modalStore.open("ConfirmPassword", {
     title: "Confirm Password",
   })) as string;
@@ -121,12 +118,10 @@ async function updateField(
 
   // request for update
   await userStore
-    .patchProfile(currentPassword, {
+    .updateUser(currentPassword, {
       [field.name]: field.value,
     })
     .then((profile) => {
-      userStore.setProfile(profile);
-      formFields.value[index].value = field.value;
       alert.value.success("Field successfully updated.");
     })
     .catch((error) => {
@@ -148,7 +143,7 @@ async function updatePassword(index: number, newPassword: string) {
 
   // request for update
   await userStore
-    .changePassword(currentPassword, newPassword)
+    .updatePassword(currentPassword, newPassword)
     .then(() => {
       alert.value.success("Password successfully updated.");
     })
@@ -172,8 +167,9 @@ async function updatePhoto(photo: any) {
   await userStore
     .uploadPhoto(password, photo)
     .then((blob) => {
+      // TODO review
       localStorage.setItem("profilePhotoURL", URL.createObjectURL(blob));
-      userStore.setPhoto(blob);
+      // userStore.setPhoto(blob);
       alert.value.sucess("Photo successfully updated.");
     })
     .catch((error) => {
@@ -202,10 +198,8 @@ async function updateEmail(index: number, newEmail: string) {
 
   // request for update
   await userStore
-    .patchEmail(currentPassword, newEmail)
+    .updateEmail(currentPassword, newEmail)
     .then((user) => {
-      userStore.setEmail(user.email);
-      formFields.value[index].value = newEmail;
       alert.value.success("Field successfully updated.");
     })
     .catch((error) => {
