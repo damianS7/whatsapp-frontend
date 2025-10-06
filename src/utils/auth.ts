bcf101dcd4c614dfe3cd517b341163d0674542f0
Damian <josepwnz@gmail.com>
2025-10-06 21:48:29 +0200

    removed unused

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,14 +2,6 @@ import { useUserStore } from "@/stores/user";
 import type { User } from "@/types/User";
 
 export function authUtils() {
-  const isCurrentUserOwner = (customerId: number) => {
-    const customer: User = useUserStore().getLoggedUser;
-    if (customer.id === customerId) {
-      return true;
-    }
-    return false;
-  };
-
   const isLoggedUser = (userId: number) => {
     const user: User = useUserStore().getLoggedUser;
     if (user.id === userId) {
@@ -19,7 +11,6 @@ export function authUtils() {
   };
 
   return {
-    isCurrentUserOwner,
     isLoggedUser,
   };
 }
